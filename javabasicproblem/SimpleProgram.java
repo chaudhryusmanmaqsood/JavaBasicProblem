b6e9ab6407b16a7d80513d24f1ef8b014d8cfd64
chaudhryusmanmaqsood <cu7683921@gmail.com>
2022-06-29 12:57:51 +0500

    Simple program add

---
+++ javabasicproblem/SimpleProgram.java
@@ -0,0 +1,11 @@
+package javabasicproblem;
+
+public class SimpleProgram {
+    public static void main(String[]args){
+
+        int a = 4;
+        int b = 6;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
